ad1e6dbe09de244be180e250bb6bdef6599a1864
Gina Fedder <ginafedder@yahoo.com>
2018-09-19 21:33:00 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -10,9 +10,9 @@ def game_hash
                                       assists:,
                                       steals:,
                                       blocks:,
-                                      slam_dunks:,
+                                      slam_dunks:
                                       
-          }}},
+          },},},
     away: {team_name: "team name", 
           colors: [team colors], 
           players: {"player name" => {number:,
@@ -22,9 +22,9 @@ def game_hash
                                       assists:,
                                       steals:,
                                       blocks:,
-                                      slam_dunks:,
+                                      slam_dunks:
                                       
-          }}}
+          },},}
   }
 end 
 
