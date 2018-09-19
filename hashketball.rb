cde44a292837160ce01b4c534c18c9355d7d3ac2
Gina Fedder <ginafedder@yahoo.com>
2018-09-19 22:31:43 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -95,7 +95,7 @@ end
 def players
   home_players = game_hash[:home][:players]
   away_players = game_hash[:away][:players]
-  total_players = home_players + away_players
+  total_players = home_players.merge(away_players)
   total_players
 end
 
