201c07d42c8ef427d9503fd7e3b84b0f8d265728
Gina Fedder <ginafedder@yahoo.com>
2018-09-19 21:34:23 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,13 +3,13 @@ def game_hash
   game = {
     home: {team_name: "Brooklyn Nets", 
           colors: ["Black", "White"], 
-          players: {"Alan Anderson" => {number:,
-                                      shoe:,
-                                      points:,
-                                      rebounds:,
-                                      assists:,
-                                      steals:,
-                                      blocks:,
+          players: {"Alan Anderson" => {number: "0",
+                                      shoe: "16",
+                                      points: "22",
+                                      rebounds: "12",
+                                      assists: "12",
+                                      steals: "3",
+                                      blocks: "1",
                                       slam_dunks:
                                       
           },},},
